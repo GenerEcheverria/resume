dfc326315768bb55b226de97b1505ea8de96ec69
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-02-19 19:19:03 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ git pull
 echo "updating repo"
 systemctl start nginx
 echo "nginx started"
+ngrok config add-authtoken 2Lvy7JNecJg6NNJVSWkX7PaND29_KmnjZkdky2o65GK5Q36y
 ngrok http 80
 echo "ngrok started"
